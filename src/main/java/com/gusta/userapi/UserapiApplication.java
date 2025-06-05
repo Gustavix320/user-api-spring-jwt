b4a190003ac4cb5414656a98e449108f2f200fce
Gustavo Santos <gustavo_santos320@hotmail.com>
2025-06-05 19:41:22 -0300

    feat: Swagger autorizado e documentado

--- src/main/java/com/gusta/userapi/UserapiApplication.java
+++ src/main/java/com/gusta/userapi/UserapiApplication.java
@@ -2,8 +2,11 @@ package com.gusta.userapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 
 @SpringBootApplication
+@OpenAPIDefinition(security = { @SecurityRequirement(name = "bearerAuth") })
 public class UserapiApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/gusta/userapi/controller/AuthController.java
+++ src/main/java/com/gusta/userapi/controller/AuthController.java
@@ -1,6 +1,8 @@
 package com.gusta.userapi.controller;
 
 import com.gusta.userapi.config.JwtUtil;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.*;
 import org.springframework.security.core.Authentication;
@@ -8,12 +10,12 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.security.core.AuthenticationException;
 
-
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/auth")
+@Tag(name = "auth-controller", description = "Autenticação e geração de token JWT")
 public class AuthController {
 
     @Autowired
@@ -23,6 +25,7 @@ public class AuthController {
     private JwtUtil jwtUtil;
 
     @PostMapping("/login")
+    @Operation(summary = "Autentica o usuário e retorna um token JWT válido")
     public Map<String, String> login(@RequestBody Map<String, String> request) {
         try {
             String email = request.get("email");
