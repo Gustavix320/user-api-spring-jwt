a150fdf4fc5b9e3a88fcac4dd1ea736e615ac08e
Gustavo Santos <gustavo_santos320@hotmail.com>
2025-06-05 20:12:17 -0300

    feat: forçar o Swagger aparecer Authorize

---
+++ src/main/java/com/gusta/userapi/config/OpenAPIConfig.java
@@ -0,0 +1,31 @@
+package com.gusta.userapi.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+            .info(new Info()
+                .title("User API")
+                .version("1.0")
+                .description("API com autenticação JWT e CRUD de usuários"))
+            .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+            .components(new io.swagger.v3.oas.models.Components()
+                .addSecuritySchemes("bearerAuth",
+                    new SecurityScheme()
+                        .name("bearerAuth")
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT")
+                )
+            );
+    }
+}
